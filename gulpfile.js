85c1609c4af280ec2575276586192a3f673548f0
Alexandru Cotici <alexandrucoticidesign@gmail.com>
2017-11-26 15:23:50 +0000

    image minification added

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,26 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var merge = require('merge-stream');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
 
 var SOURCEPATHS = {
 
 	sassSource : 'src/scss/*.scss',
 	htmlSource : 'src/*.html',
-	jsSource : 'src/js/*.js'
+	jsSource : 'src/js/**',
+	imgSource : 'src/img/**'
 }
 var APPPATH ={
 	root: 'app/',
 	css : 'app/css',
-	js : 'app/js'
+	js : 'app/js',
+	fonts : 'app/fonts',
+	img : 'app/img'
 }
 
 gulp.task('clean-html', function(){
@@ -28,15 +35,34 @@ gulp.task('clean-scripts', function(){
 });
 
 gulp.task('sass', function() {
-	return gulp.src(SOURCEPATHS.sassSource)
+	var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+	var sassFiles;
+
+
+	sassFiles = gulp.src(SOURCEPATHS.sassSource)
 		.pipe(autoprefixer())
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest(APPPATH.css));
+		return merge(bootstrapCSS, sassFiles)
+			.pipe(concat('app.css'))
+			.pipe(gulp.dest(APPPATH.css))
+});
+
+gulp.task('images', function() {
+	return gulp.src(SOURCEPATHS.imgSource)
+		.pipe(newer(APPPATH.img))
+		.pipe(imagemin())
+		.pipe(gulp.dest(APPPATH.img))
+});
+
+gulp.task('moveFonts', function(){
+	gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
+		.pipe(gulp.dest(APPPATH.fonts));
 });
 
 gulp.task('scripts', ['clean-scripts'], function() {
 	gulp.src(SOURCEPATHS.jsSource)
 		.pipe(concat('main.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest(APPPATH.js))
 });
 
@@ -53,7 +79,7 @@ gulp.task('serve', ['sass'], function() {
 	})
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images'], function() {
 	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
 	gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 	gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
